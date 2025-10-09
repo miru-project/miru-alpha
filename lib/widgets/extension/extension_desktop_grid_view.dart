e67265d357a2b675bd975f4d21bf6dc457446fa2
appdevelpo <56633229+appdevelpo@users.noreply.github.com>
2025-10-10 00:01:53 +0800

    fix: Refactor UI components and remove unused widgets

--- lib/widgets/extension/extension_desktop_grid_view.dart
+++ lib/widgets/extension/extension_desktop_grid_view.dart
@@ -4,7 +4,7 @@ import 'package:forui/forui.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:miru_app_new/model/index.dart';
 import 'package:miru_app_new/provider/extension_page_notifier_provider.dart';
-import 'package:miru_app_new/widgets/core/search_filter_card.dart';
+import 'package:miru_app_new/widgets/search/search_filter_card.dart';
 import 'package:miru_app_new/widgets/extension/extension_tile.dart';
 import 'package:miru_app_new/widgets/gridView/index.dart';
 import 'package:miru_app_new/widgets/extension/clearable_select.dart';
